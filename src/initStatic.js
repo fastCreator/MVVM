7a6552c7588cbd253a59359906e6cd064a44a30f
张果 <237625092@qq.com>
2017-05-12 14:03:16 +0800

    删除多余文件

--- src/initStatic.js
+++
@@ -1,8 +0,0 @@
-import { domTostr, strTodom, isDom } from './utils'
-export function initStatic(MVVM) {
-    MVVM.domTostr = domTostr;
-    MVVM.strTodom = strTodom;
-    MVVM.isDom = isDom;
-    //触发set的数组的方法
-}
-
